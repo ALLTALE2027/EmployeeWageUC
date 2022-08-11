089fc80330ad50d5ee5c6bdaca677765531f87b7
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-11 11:14:35 +0530

    [Darshan]: Add UC-1 Present Absent

---
+++ EmployeewageUC/PresentAbsent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageUC
+{
+    internal class PresentAbsent
+    {
+        public void presentAbsent()
+        {
+            //---------------- UC1- check Employee is present or absent -------------------//
+
+            Console.WriteLine("UC1-Employee present or absent");
+            Random random = new Random();
+            int empCheck = random.Next(0, 3); // it will give values 1 and 2 and not 3
+            int isFullTime = 1;
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+
+        }
+    }
+}
---
+++ EmployeewageUC/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeewageUC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program !!!");
+
+            //UC-1
+            PresentAbsent empPresentAbsent = new PresentAbsent();
+            empPresentAbsent.presentAbsent();
+        }
+    }
+}
