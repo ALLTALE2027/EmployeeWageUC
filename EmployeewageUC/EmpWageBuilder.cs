d11501ddddd542f219c083af7b0ee3db4629c058
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-20 20:26:44 +0530

    [Darshan]: Add Uc-9 Use Instance Variable instead of function parameters

---
+++ EmployeewageUC/EmpWageBuilder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageUC
+{
+    internal class EmpWageBuilder
+    {
+        const int isFullTime = 1;
+        const int isPartTime = 2;
+        int totalWage = 0;
+
+
+        public List<int> empDailyWages;
+        public string company;
+        private int workingDays;
+        private int wagePerHour;
+        private int totalWorkingHrs;
+
+
+
+
+        public EmpWageBuilder(string company, int workingDays, int wagePerHour, int totalWorkingHrs)
+        {
+            this.company = company;
+            this.workingDays = workingDays;
+            this.wagePerHour = wagePerHour;
+            this.totalWorkingHrs = totalWorkingHrs;
+            this.empDailyWages = new List<int>();
+        }
+
+        public void CalculateWage()
+        {
+            int empHrs;
+            int dailyWage;
+            int totalNoOfHrs = 0;
+            int totalWorkingDays = 0;
+
+            Random random = new Random();
+            while (totalWorkingDays < this.workingDays && totalNoOfHrs <= this.totalWorkingHrs)
+            {
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+
+                totalNoOfHrs += empHrs;
+                dailyWage = wagePerHour * empHrs;
+                empDailyWages.Add(dailyWage);
+                totalWage += dailyWage;
+                totalWorkingDays++;
+
+                if (totalNoOfHrs > this.totalWorkingHrs)
+                    totalNoOfHrs -= empHrs;
+            }
+
+
+            Console.WriteLine("Total No of hours are {0} and working days are {1} in {2} company", totalNoOfHrs, totalWorkingDays, this.company);
+        }
+
+        public string GetWage()
+        {
+            return "Total monthly wage of a person working in " + this.company + " company is : " + this.totalWage;
+        }
+
+
+        public void ReturnDailyWage()
+        {
+            Console.WriteLine("Daily wages of employee working in {0} company are:", this.company);
+            foreach (var i in empDailyWages)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- EmployeewageUC/Program.cs
+++ EmployeewageUC/Program.cs
@@ -7,38 +7,44 @@
             Console.WriteLine("Welcome to Employee Wage Computation Program !!!");
 
             //UC-1
-            PresentAbsent empPresentAbsent = new PresentAbsent();
-            empPresentAbsent.presentAbsent();
+            //PresentAbsent empPresentAbsent = new PresentAbsent();
+            //empPresentAbsent.presentAbsent();
 
             //UC-2
-            DailyWage empDailyWage = new DailyWage();
-            empDailyWage.dailyWage();
+            //DailyWage empDailyWage = new DailyWage();
+            //empDailyWage.dailyWage();
 
 
             //UC-3
-            PartTimeFullTime empPartTimeFullTime = new PartTimeFullTime();
-            empPartTimeFullTime.partTimeFullTime();
+            //PartTimeFullTime empPartTimeFullTime = new PartTimeFullTime();
+            //empPartTimeFullTime.partTimeFullTime();
+
             //UC-4
-            SwitchCase empSwitchCase = new SwitchCase();
-            empSwitchCase.UsingSwitch();
+            //SwitchCase empSwitchCase = new SwitchCase();
+            //empSwitchCase.UsingSwitch();
 
             //UC-5
-            WagePerMonth empWagesPerMonth = new WagePerMonth();
-            empWagesPerMonth.PerMonthWage();
+            //WagePerMonth empWagesPerMonth = new WagePerMonth();
+            //empWagesPerMonth.PerMonthWage();
 
             //UC-6
-            TotalWorkHrs100Days20 empTotalWorkHrs100Days20 = new TotalWorkHrs100Days20();
-            empTotalWorkHrs100Days20.WorkHrs100Days20();
+            //TotalWorkHrs100Days20 empTotalWorkHrs100Days20 = new TotalWorkHrs100Days20();
+            //empTotalWorkHrs100Days20.WorkHrs100Days20();
 
             //UC-7 
-            ComputeEmployeeWage computeWage=new ComputeEmployeeWage();
-            computeWage.CalculateEmpWage();
+            //ComputeEmployeeWage computeWage=new ComputeEmployeeWage();
+            //computeWage.CalculateEmpWage();
 
 
             //UC-8
-            MultipleCompany muliplCompany=new MultipleCompany();
-            muliplCompany.WageMultipleCompany("Tesla",50,25,150);
-            muliplCompany.WageMultipleCompany("SpaceX",80,20,200);
+            //MultipleCompany muliplCompany=new MultipleCompany();
+            //muliplCompany.WageMultipleCompany("Tesla",50,25,150);
+            //muliplCompany.WageMultipleCompany("SpaceX",80,20,200);
+
+            //UC-9
+            var uc9 = new EmpWageBuilder("Kingfisher", 30, 40, 100);
+            uc9.CalculateWage();
+            Console.WriteLine(uc9.GetWage());
 
         }
     }
